d54dcaaf88d7026bfc670318f3adb6397203c49b
Jacob Nakano <53596815+taikei@users.noreply.github.com>
2022-09-24 14:21:01 +1000

    Update dashboard.php

--- views/dashboard.php
+++ views/dashboard.php
@@ -7,6 +7,13 @@
 
         <!-- Page Heading -->
         <h1 class="h3 mb-4 text-gray-800">Welcome Back <?php print_r($_SESSION['id'] )?></h1>
+        
+        <?php
+            echo $_SESSION['id'];
+            echo $_SESSION['pwd'];
+            echo $_SESSION['role'];
+        ?>
+        
 
     </div>
     <!-- /.container-fluid -->
